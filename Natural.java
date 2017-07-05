67d785ae3ffd3ddf35bee050eea6a36635ba0c12
731614105005 <bawadharani96@gmail.com>
2017-07-05 09:52:42 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,16 @@
+# set8
+import java.io.*;
+ import java.util.*;
+ public class Natural
+ {
+   public static void main(String args[])throws IOException
+   {
+    Scanner sc=new Scanner(System.in);
+     int i,n=100,sum=0;
+     for(i=1;i<=n;i++)
+     {
+        sum=sum+i;
+        }
+        System.out.println(sum);
+       }
+       }
